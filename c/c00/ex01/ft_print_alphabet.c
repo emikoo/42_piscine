26f64fb0a9f69562f83020d5644d4f22f9940bda
Ademi Abdykerimova <aabdyker@c4b6c3.42berlin.de>
2023-02-16 15:48:31 +0100

    ex01 deleted additional files

--- c/c00/ex01/ft_print_alphabet.c
+++ c/c00/ex01/ft_print_alphabet.c
@@ -6,19 +6,20 @@
 /*   By: aabdyker <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/10 11:19:18 by aabdyker          #+#    #+#             */
-/*   Updated: 2023/02/10 11:40:30 by aabdyker         ###   ########.fr       */
+/*   Updated: 2023/02/16 13:52:45 by aabdyker         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void ft_print_alphabet(void)
+void	ft_print_alphabet(void)
 {
-	char alphabet = 'a';
+	char	alphabet;
 
-	while(alphabet <= 'z')
+	alphabet = 'a';
+	while (alphabet <= 'z')
 	{
 		write(1, &alphabet, 1);
 		alphabet++;
 	}
-} 
+}
