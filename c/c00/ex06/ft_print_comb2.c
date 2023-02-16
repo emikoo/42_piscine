1a323a73ed320f539cf2985d044aacc9d3a68130
Ademi Abdykerimova <aabdyker@c4b6c3.42berlin.de>
2023-02-16 17:44:06 +0100

    wrong

--- c/c00/ex06/ft_print_comb2.c
+++ c/c00/ex06/ft_print_comb2.c
@@ -6,7 +6,7 @@
 /*   By: aabdyker <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/14 20:05:33 by aabdyker          #+#    #+#             */
-/*   Updated: 2023/02/15 16:45:34 by aabdyker         ###   ########.fr       */
+/*   Updated: 2023/02/16 14:32:36 by aabdyker         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,25 @@
 
 void	ft_print_res(char a, char b, char c, char d)
 {
-	write(1, &a, 1);
-	write(1, &b, 1);
-	write(1, "\0 ", 3);
-	write(1, &c, 1);
-	write(1, &d, 1);
-	write(1, ", ", 2);
+	if (a == c && b == d)
+		d++;
+	if (a == '9' && b == '8' && c == '9' && d == '9')
+	{
+		write(1, &a, 1);
+		write(1, &b, 1);
+		write(1, " ", 1);
+		write(1, &c, 1);
+		write(1, &d, 1);
+	}
+	else
+	{
+		write(1, &a, 1);
+		write(1, &b, 1);
+		write(1, " ", 1);
+		write(1, &c, 1);
+		write(1, &d, 1);
+		write(1, ", ", 2);
+	}
 }
 
 void	ft_print_dec(char a, char b, char c, char d)
@@ -30,6 +43,8 @@ void	ft_print_dec(char a, char b, char c, char d)
 		{
 			ft_print_res(a, b, c, d);
 			d++;
+			if (a == c && b == d)
+				d++;
 		}
 		d = '0';
 		c++;
@@ -54,7 +69,7 @@ void	ft_print_comb2(void)
 			ft_print_dec(a, b, c, d);
 			c = '0';
 			b++;
-			d = '2';
+			d = b + 1;
 		}
 		b = '0';
 		a++;
